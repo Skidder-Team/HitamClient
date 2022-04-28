87b2b187081eded6909c43418333342e1f644672
Kliwyr <78771924+Kliwyr6969@users.noreply.github.com>
2022-04-28 05:10:09 +0000

    Discord RPC

--- src/main/java/net/ccbluex/liquidbounce/features/special/DiscordRPC.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/special/DiscordRPC.kt
@@ -42,7 +42,7 @@ object DiscordRPC {
         val builder = RichPresence.Builder()
         builder.setStartTimestamp(timestamp)
         builder.setLargeImage("epep")
-        builder.setDetails("Playing Block Game ${LiquidBounce.MINECRAFT_VERSION}")
+        builder.setDetails("Playing Hitam Client V-${LiquidBounce.MINECRAFT_VERSION}")
         ServerUtils.getRemoteIp().also {
             builder.setState(if(it.equals("idling", true)) "Currently not skidding" else "Skidding on $it ")
         }
--- src/main/java/net/ccbluex/liquidbounce/ui/client/altmanager/GuiAltManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/altmanager/GuiAltManager.kt
@@ -196,7 +196,7 @@ class GuiAltManager(private val prevGui: GuiScreen) : GuiScreen() {
         var randomAltField = GuiTextField(2, Minecraft.getMinecraft().fontRendererObj, 0, 0, 0, 0)
 
         init {
-            randomAltField.text = "F%nD%nP%n_%s%s%s"
+            randomAltField.text = "HITAM_%n%n%n%s"
             randomAltField.maxStringLength = Int.MAX_VALUE
         }
 
