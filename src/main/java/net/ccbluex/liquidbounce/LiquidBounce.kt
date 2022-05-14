8f315c85a77e44a4e047134e29b19c49d1b1e1a1
Necro <Imhuman2506@gmail.com>
2022-05-14 16:28:08 +0700

    Gradle fix

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -66,7 +66,7 @@ object LiquidBounce {
 
     // 自动读取客户端版本
     @JvmField
-    val CLIENT_VERSION = "v1.0.0"
+    val CLIENT_VERSION = "v2.0.0"
     @JvmField
     val CLIENT_BRANCH = (gitInfo["git.branch"] ?: "unknown").let {
         if(it == "main") "dev" else it
