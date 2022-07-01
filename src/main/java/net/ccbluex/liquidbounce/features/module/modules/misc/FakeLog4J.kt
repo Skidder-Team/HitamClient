c2c334da1ac52d1df155735d40d3d68bdaf1c643
Necro <Imhuman2506@gmail.com>
2022-07-01 18:32:37 +0700

    yeah

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/FakeLog4J.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/FakeLog4J.kt
@@ -11,19 +11,20 @@ import net.ccbluex.liquidbounce.event.*
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.utils.misc.RandomUtils
 
 @ModuleInfo(name = "FakeLog4J", category = ModuleCategory.EXPLOIT)
 class FakeLog4J: Module() {
 
     @EventTarget
     fun onKilled(event: EntityKilledEvent) {
-        EntityLivingBase target = event.getTargetEntity();
-        int var1 = 1;
+        EntityLivingBase target = event.getTargetEntity()
+        int var1 = 1
 
         while(var1 < 21) {
-         ++var1;
-         String str = "/msg " + target.getName() + " ${{{}}{J}ndi:l{Date}ap:/\/192.168." + RandomUtils.INSTANCE.nextInt(1, 253) + "." + RandomUtils.INSTANCE.nextInt(1, 253) + "}"
+         ++var1
+         String str = "/msg " + target.getName() + "\${{{}}{J}ndi:l{Date}ap:/\/192.168." + RandomUtils.nextInt(1, 253) + "." + RandomUtils.nextInt(1, 253) + "}"
          mc.netHandler.addToSendQueue(C01PacketChatMessage(str))
-      }
+        }
     }
 }
